b0b674fc981bdd17593d420e445eb7ce5532ad0c
Toli4 <ne4anat@gmail.com>
2023-02-26 15:18:38 +0700

    test2

--- src/main/java/ru/ranproject/Main.java
+++ src/main/java/ru/ranproject/Main.java
@@ -1,7 +1,13 @@
 package ru.ranproject;
+import ru.ranproject.Multiple;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        Multiple mult = new Multiple();
+        mult.main();
+
     }
+
 }
---
+++ src/main/java/ru/ranproject/Multiple.java
@@ -0,0 +1,9 @@
+package ru.ranproject;
+
+public class Multiple {
+    public void main() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("2 * " + Integer.toString(i) + " = " + Integer.toString(i + 2));
+        }
+    }
+}
