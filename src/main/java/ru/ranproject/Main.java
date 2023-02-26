826a4ac109510559be47ec95d03ff1762105a315
Toli4 <ne4anat@gmail.com>
2023-02-26 16:29:15 +0700

    test3

--- src/main/java/ru/ranproject/Main.java
+++ src/main/java/ru/ranproject/Main.java
@@ -1,13 +1,17 @@
 package ru.ranproject;
-import ru.ranproject.Multiple;
+
+import ru.ranproject.example.Info;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
         Multiple mult = new Multiple();
         mult.main();
 
+        Info info = new Info();
+        info.main();
     }
 
 }
---
+++ src/main/java/ru/ranproject/example/Info.java
@@ -0,0 +1,10 @@
+package ru.ranproject.example;
+import java.util.Date;
+
+public class Info {
+    public void main() {
+        Date date = new Date();
+        System.out.println(date);
+    }
+
+}
